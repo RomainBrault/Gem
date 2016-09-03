969931ce72f5dc9183d62a63f3aeaa21a5dc9cba
RomainBrault <ro.brault@gmail.com>
2016-09-03 13:08:46 +0200

    Travis

--- build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Romain/Gem/tests
-# Build directory: /Users/Romain/Gem/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests.dimensions.base "/Users/Romain/Gem/build/tests/tests.dimensions.base")
-add_test(tests.io.print "/Users/Romain/Gem/build/tests/tests.io.print")
-add_test(tests.main "/Users/Romain/Gem/build/tests/tests.main")
